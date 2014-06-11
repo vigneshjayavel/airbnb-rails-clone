6d840050085b82d1de450709f9905a17b25d4bd6
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-06-11 15:17:46 +0530

    disable edit,delete buttons for others listings

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,8 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
+  map.home 'home', :controller => "pages", :action => "index"
+
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
   map.resource :account, :controller => "users"
