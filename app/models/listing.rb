ff53c708fba8b3f0352dc93a3914fb0f18e6046b
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-06-16 16:09:37 +0530

    validations for listings creation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,21 @@
 class Listing < ActiveRecord::Base
 
+	# Associations
   belongs_to :user
-  
+
   named_scope :not_belonging_to_current_user, lambda { |current_user_id| 
     { :conditions => ["user_id != ?", current_user_id] }
   }
 
   has_many :reservations
-  
+
+  # Validations
+  validates_presence_of :name, :address
+  validate :validate_availability
+
+  private
+  def validate_availability
+  	errors.add("Availability ") if self.availability_from >= self.availability_to
+  end
 end
  	
