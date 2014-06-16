d46e3a3d8de7d9720cc30d2df283a4f8116fbf62
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-06-16 17:41:26 +0530

    reservations controller cleanup

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,54 +8,35 @@ class ReservationsController < ApplicationController
 		@reservation = Reservation.find(params[:id])
 	end
 
-
-  # GET /reservations/1/edit
   def edit
     @reservation = Reservation.find(params[:id])
   end
 
-  # POST /reservations
-  # POST /reservations.xml
   def create
     @reservation = current_user.reservations.new(params[:reservation])
     
-    respond_to do |format|
-      if @reservation.save
-        format.html { redirect_to(@reservation, :notice => 'Reservation was successfully created.') }
-        format.xml  { render :xml => @reservation, :status => :created, :location => @reservation }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @reservation.errors, :status => :unprocessable_entity }
-      end
+    if @reservation.save
+      redirect_to(@reservation, :notice => 'Reservation was successfully created.') 
+    else
+      render :action => "new" 
     end
+
   end
 
-  # PUT /reservations/1
-  # PUT /reservations/1.xml
   def update
     @reservation = Reservation.find(params[:id])
 
-    respond_to do |format|
-      if @reservation.update_attributes(params[:reservation])
-        format.html { redirect_to(@reservation, :notice => 'Reservation was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @reservation.errors, :status => :unprocessable_entity }
-      end
+    if @reservation.update_attributes(params[:reservation])
+      redirect_to(@reservation, :notice => 'Reservation was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /reservations/1
-  # DELETE /reservations/1.xml
   def destroy
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(reservations_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(reservations_url)
   end
 
 end
