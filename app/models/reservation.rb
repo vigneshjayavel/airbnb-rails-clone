1e33519897cdc1ac9164bb4743860c627ebd4cee
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-06-16 18:17:55 +0530

    reservation period validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,9 @@ class Reservation < ActiveRecord::Base
 
 	#Validations
 	validates_presence_of :guests_count
+	validate :reservation_period_validation
 
+	private
 	def reservation_period_validation
 		errors.add("Reservation period ") if self.check_in >= self.check_out
 	end
