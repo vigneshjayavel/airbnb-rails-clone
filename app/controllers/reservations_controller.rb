ebdc39aaabd83dd97cdb6e2c35ca5dc024e7b5a3
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-06-16 18:42:47 +0530

    reservation of expired listings validation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,12 +14,17 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = current_user.reservations.new(params[:reservation])
-    
-    if @reservation.save
-      redirect_to(@reservation, :notice => 'Reservation was successfully created.') 
-    else
-      flash[:notice] = @reservation.errors.full_messages.to_sentence
+    listing = Listing.find(params[:reservation][:listing_id])
+    if Date.today > listing.availability_from && Date.today > listing.availability_to
+      flash[:notice] = "The listing's availability has expired"
       redirect_to(reserve_listing_url params[:reservation][:listing_id])
+    else
+      if @reservation.save
+        redirect_to(@reservation, :notice => 'Reservation was successfully created.') 
+      else
+        flash[:notice] = @reservation.errors.full_messages.to_sentence
+        redirect_to(reserve_listing_url params[:reservation][:listing_id])
+      end
     end
 
   end
@@ -37,8 +42,13 @@ class ReservationsController < ApplicationController
 
   def destroy
     @reservation = Reservation.find(params[:id])
-    @reservation.destroy
-    redirect_to(reservations_url)
+    listing = Listing.find(@reservation.listing_id)
+    if Date.today < @reservation.check_in + listing.notice_period 
+      @reservation.destroy
+      redirect_to(reservations_url)
+    else 
+      redirect_to(@reservation, :notice => "You cannot cancel the reservation. Minimum notice period is #{listing.notice_period} days")
+    end
   end
 
 end
