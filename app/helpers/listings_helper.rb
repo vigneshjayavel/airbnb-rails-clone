8b2d0e48231017ba9692c86992c869e35f45c0dd
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-06-16 13:00:19 +0530

    select options populate for home and room types

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,11 @@
 module ListingsHelper
+
+	def get_home_types
+		return ["apartment", "house", "villa", "dorm"]
+	end
+
+	def get_room_types
+		return ["entire home", "private room", "shared room"]		
+	end
+
 end
